8ab843f57b13e8f719cdc25a787a4f467311236e
ontariodf <robin@onlinedivorceforms.ca>
2020-07-13 13:52:47 -0400

    working YAYAYAYAAYA; going to test removing headers form api

--- api/main.go
+++ api/main.go
@@ -42,10 +42,9 @@ func main() {
 
 func setUpResponse(w *http.ResponseWriter) {
 	(*w).Header().Set("Content-Type", "application/json")
-	(*w).Header().Set("MyApiHeader", "Hello-from-the-api")
-	(*w).Header().Set("Access_control-Allow_origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	// (*w).Header().Set("Access_control-Allow_origin", "*")
+	// (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	// (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func testBackend(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
