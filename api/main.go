08e98f0925f7b418b2f74fd12a3c1134e3896bae
ontariodf <robin@onlinedivorceforms.ca>
2020-07-13 10:43:02 -0400

    added volumes to Dockerun 9

--- api/main.go
+++ api/main.go
@@ -45,7 +45,6 @@ func setUpResponse(w *http.ResponseWriter) {
 	(*w).Header().Set("MyApiHeader", "Hello-from-the-api8")
 	(*w).Header().Set("Access_control-Allow_origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Allow", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
