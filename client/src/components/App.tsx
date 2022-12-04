d78f88439ec4ed49c9070bfc65121690c3c68a93
Robin Erickson <re@incuboweb.ca>
2022-12-04 07:22:15 -0500

    add log

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -18,6 +18,7 @@ const App: React.FC = () => {
     useEffect(() => {
         setLogoImg(document.getElementById('logo-img'));
     }, []);
+    console.log('====> F TEST',)
 
     window.onscroll = () => {
         const width = window.innerWidth;
